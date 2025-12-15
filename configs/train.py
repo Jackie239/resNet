e100f3c49a761b00c61dd16e4f1d09d6ed9c1571
jackie_honor <jackie_honor>
2025-12-15 14:56:25 +0800

    修复了 tensorboard 不记录的bug

--- configs/train.py
+++ configs/train.py
@@ -7,7 +7,7 @@ parser.add_argument('--dataPath', type=str, default='./data/preTrainedModel/resn
                     help='path to dataset')
 parser.add_argument('--shuffle', type=bool, default=True,
                     help='shuffle the dataset')
-parser.add_argument('--num_workers', type=int, default=12,
+parser.add_argument('--num_workers', type=int, default=0,
                     help='number of workers for data loading')
 # model
 parser.add_argument('--pretrained', type=bool, default=True,
@@ -20,18 +20,18 @@ parser.add_argument('--num_epochs', type=int, default=10,
 parser.add_argument('--lr', type=float, default=0.001,
                     help='learning rate')
 # device
-parser.add_argument('--device', type=str, default='cuda',
+parser.add_argument('--device', type=str, default='cpu',
                     help='cpu or cuda')
 # log
 parser.add_argument('--use_tensorboard', type=bool, default=True,
                     help='whether to use tensorboard to log training status')
-parser.add_argument('--tensorboard_dir', type=str, default='./tensorboard/',
+parser.add_argument('--tensorboard_dir', type=str, default='./output/tensorboard/',
                     help='path to save tensorboard logs')
 parser.add_argument('--log_interval', type=int, default=10,
                     help='number of steps between logging training status')
 # checkpoint
-parser.add_argument('--output_dir', type=str, default='./output/',
-                    help='path to save outputs')
+parser.add_argument('--checkpoint_dir', type=str, default='./output/checkpoints/',
+                    help='path to save checkpoints')
 parser.add_argument('--session', type=int, default=1,
                     help='session id for this training')
 parser.add_argument('--save_interval', type=int, default=2,
