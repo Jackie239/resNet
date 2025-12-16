a1d97a0943428f11d50697cc21e76fc87c981567
xurujun <xurujun@iyobiai.com>
2025-12-16 18:28:08 +0800

    修复了log的间隔大小和测试期间不相等的bug

--- configs/test.py
+++ configs/test.py
@@ -8,7 +8,7 @@ parser.add_argument('--dataPath', type=str, default='./data',
 parser.add_argument('--num_workers', type=int, default=12,
                     help='number of workers for data loading')
 # testing
-parser.add_argument('--batchSize', type=int, default=128,
+parser.add_argument('--batchSize', type=int, default=64,
                     help='input batch size for training')
 # device
 parser.add_argument('--device', type=str, default='cuda',
@@ -26,7 +26,7 @@ parser.add_argument('--checkpoint_dir', type=str, default='./output/checkpoints/
                     help='path to save checkpoints')
 parser.add_argument('--checkSession', type=int, default=1,
                     help='session id for this training')
-parser.add_argument('--checkEpoch', type=int, default=1,
+parser.add_argument('--checkEpoch', type=int, default=2,
                     help='epoch id for this training')
 parser.add_argument('--checkPoint', type=int, default=469,
                     help='point id for this training')
